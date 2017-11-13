45a1eaa3cfe1736d641929a4241f64d36d624403
horaciocoronel <arielc@gmail.com>
2017-11-13 16:56:50 -0500

    Finish exercise

--- js/main.js
+++ js/main.js
@@ -5,14 +5,37 @@ document.addEventListener("DOMContentLoaded",
   modalWindow = document.querySelector('.modal');
   closeBtn = document.querySelector('.close');
   submitBtn = document.querySelector('.submit');
+  userField = document.getElementById('user');
+  passwordField = document.getElementById('pass');
  //
-  signInBtn.addEventListener('click', function() {
+  signInBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
     modalWindow.style.display = 'block';
-});
-closeBtn.addEventListener('click', function() {
-  modalWindow.style.display = 'none';
-});
+  });
+  closeBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    modalWindow.style.display = 'none';
+  });
+  submitBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    userField.className = "error";
+    passwordField.className = "error";
+  });
+  userField.addEventListener('click', function(event) {
+    event.stopPropagation();
+    userField.className = "";
+    passwordField.className = "";
 
+  });
+  passwordField.addEventListener('click', function(event) {
+    event.stopPropagation();
+    userField.className = "";
+    passwordField.className = "";
+  });
+  modalWindow.addEventListener('click', function(event) {
+    event.stopPropagation();
+    modalWindow.style.display = 'none';
+  });
   }
  //
 );
